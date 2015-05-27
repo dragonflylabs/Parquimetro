ad37f3271029c32a37e5b6162bdb2a03efe0b315
Daniel García Alvarado <danigarciaalva@hotmail.com>
2015-05-26 22:59:31 -0500

    Menu added

--- app/src/main/java/dflabs/io/parquimetro/MainActivity.java
+++ app/src/main/java/dflabs/io/parquimetro/MainActivity.java
@@ -1,38 +1,81 @@
 package dflabs.io.parquimetro;
 
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
+
+import java.util.HashMap;
+
+import dflabs.io.parquimetro.fragments.MenuFragment;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends SlidingFragmentActivity {
 
+    private Fragment mContent;
+
+    @SuppressLint("NewApi")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        SlidingMenu sm = getSlidingMenu();
+        sm.setShadowWidthRes(R.dimen.shadow_width);
+        sm.setShadowDrawable(R.drawable.shadow);
+        sm.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        sm.setFadeDegree(0.35f);
+        sm.setMode(SlidingMenu.LEFT);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        if (savedInstanceState != null)
+            mContent = getSupportFragmentManager().getFragment(savedInstanceState, "mContent");
+        if (mContent == null)
+            mContent = new Fragment();
+
+        setContentView(R.layout.content_frame);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content_frame, mContent)
+                .commit();
+        setBehindContentView(R.layout.menu_frame);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.menu_frame, new MenuFragment())
+                .commit();
+        getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void switchContent(final Fragment fragment, boolean asking) {
+        if (mContent.getClass() == fragment.getClass()) {
+            getSlidingMenu().showContent();
+            return;
         }
-
-        return super.onOptionsItemSelected(item);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .addToBackStack(null)
+                .replace(R.id.content_frame, fragment)
+                .commit();
+        getSlidingMenu().showContent();
+        mContent = fragment;
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
---
+++ app/src/main/java/dflabs/io/parquimetro/fragments/MenuFragment.java
@@ -0,0 +1,9 @@
+package dflabs.io.parquimetro.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by danielgarcia on 5/26/15.
+ */
+public class MenuFragment extends Fragment {
+}
